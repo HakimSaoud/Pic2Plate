abe098d6809a6db696bdd685f4e113a7b9d31662
Hakim Saoud <66874834+HakimSaoud@users.noreply.github.com>
2025-04-01 03:02:33 +0100

    fix some bugs

--- lib/components/base_auth.dart
+++ lib/components/base_auth.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class BaseAuth {
-  static const String baseUrl = 'http://192.168.100.39:3000';
-  //static const String baseUrl = 'http://localhost:3000';
+  //static const String baseUrl = 'http://192.168.100.39:3000';
+  static const String baseUrl = 'http://localhost:3000';
   static String? _accessToken;
   static String? _refreshToken;
   static String? _username;
@@ -24,8 +24,6 @@ class BaseAuth {
     String username,
     String email,
     String password,
-
-    // Add this field
   ) async {
     try {
       final response = await http.post(
